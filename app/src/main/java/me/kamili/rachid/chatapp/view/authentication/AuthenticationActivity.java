39dc98a399244bca9d81e1c19393a0834fd9a84e
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-19 00:17:50 -0400

    Adding Auth logic with Email+Google+Facebook+Twitter

--- app/src/main/java/me/kamili/rachid/chatapp/view/authentication/AuthenticationActivity.java
+++ app/src/main/java/me/kamili/rachid/chatapp/view/authentication/AuthenticationActivity.java
@@ -1,15 +1,90 @@
 package me.kamili.rachid.chatapp.view.authentication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.util.ExtraConstants;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 import me.kamili.rachid.chatapp.R;
+import me.kamili.rachid.chatapp.view.users.UsersActivity;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                new AuthUI.IdpConfig.TwitterBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            // already signed in
+            Toast.makeText(this, "Already signed in", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent().setClass(this, UsersActivity.class);
+            startActivity(intent);
+        } else {
+            // not signed in
+            Toast.makeText(this, "Not signed in", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+
+                Intent intent = new Intent().setClass(this, UsersActivity.class)
+                        .putExtra(ExtraConstants.IDP_RESPONSE, response);
+                startActivity(intent);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "User pressed back button", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No network", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(this, "Sign-in error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/me/kamili/rachid/chatapp/view/users/UsersActivity.java
@@ -0,0 +1,32 @@
+package me.kamili.rachid.chatapp.view.users;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import me.kamili.rachid.chatapp.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+
+    public void onLogOut(View view) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                    }
+                });
+
+    }
+}
